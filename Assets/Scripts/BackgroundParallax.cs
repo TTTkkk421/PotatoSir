9d2e28b94f7dda7752c47130f26c01935e2cfeca
TTTkkk421 <872980360@qq.com>
2019-03-01 14:29:30 +0800

    Add origin project
    Add origin project

---
+++ Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    public Transform[] backgrounds; //所有要背景补偿移动的背景的数组
+
+    public float parallaxScale; //运动补偿系数
+
+    public float parallaxReductionFactor; //与摄像机运动的关联程度
+
+    public float smoothing; //背景运动平滑程度
+
+    private Transform cam; //摄像机运动位置
+
+    private Vector3 previousCamPos; //摄像机上一帧的位置
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    void Start()
+    {
+        previousCamPos = cam.position;
+    }
+
+    
+    void Update()
+    {
+        float parallax = (previousCamPos.x - cam.position.x) * parallaxScale; //定义数组中各元素在x轴上的运动量
+
+        //数组中每一个背景都要移动
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax * (i * parallaxReductionFactor + 1);//x轴上的移动量目的地
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z); //整体目的地坐标
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);//插值平滑过渡
+        }
+
+        previousCamPos = cam.position; // 记录摄像机的位置为下一帧使用
+
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float xMargin = 1f; //摄像机与角色之间的x距离（不会超过此距离）
+    public float yMargin = 1f; //摄像机与角色之间的y距离
+    public float xSmooth = 8f; 
+    public float ySmooth = 8f;
+    public Vector2 MaxXAndY; //在此范围之内，摄像机才会跟随主角一起移动,摄像机运动范围被限定在这里面
+    public Vector2 MinXAndY;
+
+    private Transform player; //玩家位置
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    bool CheckXMargin()
+    {
+        return Mathf.Abs(transform.position.x - player.position.x) > xMargin;//检测x轴上的距离是否超出
+    }
+
+    bool CheckYMargin()
+    {
+        return Mathf.Abs(transform.position.y - player.position.y) > yMargin;//检测y轴上的距离是否超出
+    }
+
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+
+    void TrackPlayer()
+    {
+        float targetX = transform.position.x;
+        float targetY = transform.position.y;
+
+        //超出距离，进行移动
+        if (CheckXMargin())
+        {
+            targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.deltaTime); //目标坐标x轴
+        }
+
+        if (CheckYMargin())
+        {
+            targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.deltaTime); //目标坐标Y轴
+        }
+
+        //如果目标位置超出了我们限定的摄像机移动范围，目标位置只能移动到限定的边界
+        targetX = Mathf.Clamp(targetX, MinXAndY.x, MaxXAndY.x);
+        targetY = Mathf.Clamp(targetY, MinXAndY.y, MaxXAndY.y);
+
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+}
